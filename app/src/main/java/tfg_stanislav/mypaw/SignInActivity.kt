4a3a6b289fe1822d32b54e7cde21d537b690d499
Stanislav Krastev <stani.mk22@gmail.com>
2023-04-12 16:41:44 +0200

    Creada la pantalla de inicio de sesión

---
+++ app/src/main/java/tfg_stanislav/mypaw/SignInActivity.kt
@@ -0,0 +1,56 @@
+package tfg_stanislav.mypaw
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import tfg_stanislav.mypaw.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignInBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        firebaseAuth = FirebaseAuth.getInstance()
+//        binding.textView.setOnClickListener {
+//            val intent = Intent(this, SignUpActivity::class.java)
+//            startActivity(intent)
+//        }
+
+        binding.button.setOnClickListener {
+            val email = binding.emailEt.text.toString()
+            val pass = binding.passET.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                firebaseAuth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, "Credenciales incorrectos, revisa los datos.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Hay campos vacíos.", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+//        if(firebaseAuth.currentUser != null){
+//            val intent = Intent(this, MainActivity::class.java)
+//            startActivity(intent)
+//        }
+    }
+}
